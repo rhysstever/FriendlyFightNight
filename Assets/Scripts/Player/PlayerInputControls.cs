b9b268375f2514861a0d5bb778c9ace906786ef2
Rhys Stever <rhys@stever.com>
2024-11-24 01:08:27 -0500

    Add rest of special animations
    Special animations do not animate in game: TODO

--- Assets/Scripts/Player/PlayerInputControls.cs
+++ Assets/Scripts/Player/PlayerInputControls.cs
@@ -70,6 +70,7 @@ public class PlayerInputControls : MonoBehaviour {
     private void Special(InputAction.CallbackContext obj) {
         Transform playerChild = transform.GetChild(0);
         playerChild.GetComponent<PlayerCombat>().ActiveAbility.UseSpecial();
+        //playerChild.GetComponent<Animator>().SetBool("isUsingSpecial", true);
     }
 
     private void PauseGame(InputAction.CallbackContext obj) {
